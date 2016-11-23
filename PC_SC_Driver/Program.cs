a8e6251c1113aac75cac4ef4b6c3bc77b18e7bae
mi1vus <milvu@e1.ru>
2016-11-23 11:06:27 +0500

    косметика

--- PC_SC_Driver/Program.cs
+++ PC_SC_Driver/Program.cs
@@ -17,7 +17,6 @@ using CustomMifareReader;
 
 public static class PC_SC_Drv
 {
-
     private static SmartCardReader _reader = null;
     private static MiFareCard _card = null;
     private static MiFareCard _localCard = null;
