abd4c5e417c48805509fae9a2732afdbac9f6f6a
Max <Max@DESKTOP-UGEV1NB>
2017-03-02 15:18:12 +0500

    проект

--- Utilities/Constants.cs
+++ Utilities/Constants.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CoverConstants
+﻿namespace CoverConstants
 {
     public enum ErrorCodes
     {
